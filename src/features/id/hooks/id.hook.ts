6515a770d81c5538280f50a3a4521396a6fba94b
Bit-Barron <bit.barron22@gmail.com>
2025-12-07 04:10:43 +0100

    Refactor: Simplify ID management in translation JSON files and improve error handling in hooks

--- src/features/id/hooks/id.hook.ts
+++ src/features/id/hooks/id.hook.ts
@@ -10,38 +10,6 @@ export const useIdHook = () => {
   const { selectedNode } = useEditorStore();
 
   const addIdToJson = async (value: string) => {
-    if (!parsedProject || !value) return null;
-    const TRANSLATION_FILES =
-      parsedProject.translation_packages[0].translation_urls;
-    for (let path in TRANSLATION_FILES) {
-      const filePath = TRANSLATION_FILES[path].path; // Translation file path
-      const jsonFilePath = `${parsedProject.source_root_dir}${filePath}`; // full path to the translation JSON files
-      const content = await readTextFile(jsonFilePath); // Read the content of the translation JSON file
-      const obj = parseJson(content); // Parse the JSON content
-      const getKey = value.split(".")[0]; // Extract the key from the value
-      const getValue = value.split(".")[1]; // Extract the subkey from the value (after the dot)
-
-      const entries = Object.entries(obj) // Convert object to array of entries
-        .map(([key, val]) => ({ key, val })) // Map entries to objects with key and val properties
-        .find((e) => e.key === getKey); // Find the entry with the matching key
-
-      if (entries && getValue !== "") {
-        const addToVal = entries.val as Record<string, unknown>; // Get the values of the object
-        addToVal[getValue] = ""; // Add the new subkey with an empty string as value
-        obj[getKey] = addToVal as typeof entries.val; // Update the original object with the modified values
-        const updatedContent = JSON.stringify(obj, null, 2); // Convert the updated object back to JSON string
-        await writeTextFile(jsonFilePath, updatedContent); // Write the updated content back to the file
-      }
-    }
-
-    const updatedProject = await updateProjectFolderStructure(parsedProject); // Update the project folder structure
-    setParsedProject(updatedProject as ParsedProject); // Update the parsed project in the store
-
-    return updatedProject;
-  };
-
-  const removeIdFromJson = async () => {
-    if (!parsedProject || !selectedNode) return null;
     const TRANSLATION_FILES =
       parsedProject.translation_packages[0].translation_urls;
 
@@ -50,26 +18,55 @@ export const useIdHook = () => {
       const jsonFilePath = `${parsedProject.source_root_dir}${filePath}`;
       const content = await readTextFile(jsonFilePath);
       const obj = parseJson(content);
-      const getKey = selectedNode.data.id.split(".")[0];
-      const getValue = selectedNode.data.id.split(".")[1];
+      const splitSelectedNode = selectedNode.data.id.split(".");
+      let current: any = obj;
+      let parrent: any = "";
 
-      const entries = Object.entries(obj)
-        .map(([key, val]) => ({ key, val }))
-        .find((e) => e.key === getKey);
+      for (let i = 0; i < splitSelectedNode.length; i++) {
+        current = current[splitSelectedNode[i]];
 
-      if (entries && getValue !== "") {
-        const removeFromVal = entries.val as Record<string, unknown>; // Get the values of the object
-        delete removeFromVal[getValue]; // Remove the subkey from the values
-        obj[getKey] = removeFromVal as typeof entries.val; // Update the original object with the modified values
-        const updatedContent = JSON.stringify(obj, null, 2); // Convert the updated object back to JSON string
-        await writeTextFile(jsonFilePath, updatedContent); // Write the updated content back to the file
+        if (typeof current === "object") {
+          parrent = current;
+          current[value] = "";
+        }
+        parrent[value[i]];
       }
+
+      const updateContent = JSON.stringify(obj, null, 2);
+      writeTextFile(jsonFilePath, updateContent);
     }
 
-    const updatedProject = await updateProjectFolderStructure(parsedProject); // Update the project folder structure
-    setParsedProject(updatedProject as ParsedProject); // Update the parsed project in the store
+    const updatedProject = await updateProjectFolderStructure(parsedProject);
+    setParsedProject(updatedProject as ParsedProject);
+  };
 
-    return updatedProject;
+  const removeIdFromJson = async () => {
+    const TRANSLATION_FILES =
+      parsedProject.translation_packages[0].translation_urls;
+
+    for (let path in TRANSLATION_FILES) {
+      const filePath = TRANSLATION_FILES[path].path;
+      const jsonFilePath = `${parsedProject.source_root_dir}${filePath}`;
+      const content = await readTextFile(jsonFilePath);
+      const obj = parseJson(content);
+
+      const splitSelectedNode = selectedNode.data.id.split(".");
+
+      let current: any = obj;
+      let parrent: any = "";
+
+      for (let i = 0; i < splitSelectedNode.length; i++) {
+        parrent = current;
+        current = current[splitSelectedNode[i]];
+      }
+
+      delete parrent[splitSelectedNode[splitSelectedNode.length - 1]];
+      const updatedContent = JSON.stringify(obj, null, 2);
+
+      writeTextFile(jsonFilePath, updatedContent);
+    }
+    const updatedProject = await updateProjectFolderStructure(parsedProject);
+    setParsedProject(updatedProject as ParsedProject);
   };
 
   return { addIdToJson, removeIdFromJson };
