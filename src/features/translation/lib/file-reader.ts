bcdcbd08b3a93390021e8e676964e4931c86796a
Bit-Barron <bit.barron22@gmail.com>
2025-12-10 10:43:49 +0100

    Remove helper.txt from tracking

--- src/features/translation/lib/file-reader.ts
+++ src/features/translation/lib/file-reader.ts
@@ -4,8 +4,8 @@ import parseJson from "parse-json";
 export const readTranslationFile = async (
   rootDir: string,
   path: string
-): Promise<Record<string, any>> => {
+): Promise<Record<string, string>> => {
   const fullPath = `${rootDir}${path}`;
   const content = await readTextFile(fullPath);
-  return parseJson(content);
+  return parseJson(content) as Record<string, string>;
 };
